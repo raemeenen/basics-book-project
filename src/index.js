5f5cfa39c7278f39cebc72925b9c55528bfc405e
raemeenen <107419219+raemeenen@users.noreply.github.com>
2022-07-26 11:26:39 -0500

    created separate files for css and js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function gatherInfo() {
+  let name = prompt("What is your first name?");
+  let email = prompt("What is your email address?");
+  let genre = prompt("What is your favorite book genre?");
+  let heading = document.querySelector("h1");
+
+  if (genre.length > 0) {
+  heading.innerHTML =
+  "Thank you, " + name + "! We will send recommendations to " + email;
+  } else {
+  heading.innerHTML =
+  "I'm sorry, " + name + ", that we couldn't help you.";
+  }
+  }
+  
+  let getBooks = document.querySelector("button");
+  getBooks.addEventListener("click", gatherInfo);
